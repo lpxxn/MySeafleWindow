e1f5fb60392c9afc7e5528805021cbacab7e02c4
lipeng <mi_duo@live.com>
2014-05-16 17:38:21 +0800

    细节

--- MySeafileWindow/flodsview.cpp
+++ MySeafileWindow/flodsview.cpp
@@ -11,22 +11,22 @@ FlodsView::FlodsView(QWidget *parent) :
     ui->setupUi(this);
     setWindowTitle(tr("File Viewer"));
     LineEditDir = new QLineEdit();
-       LineEditDir->setText("/");
-       ListWidgetFile = new QListWidget();
+    LineEditDir->setText("/");
+    ListWidgetFile = new QListWidget();
 
-           QVBoxLayout *vbLayout = new QVBoxLayout( this );
-           vbLayout->addWidget( LineEditDir );
-           vbLayout->addWidget( ListWidgetFile );
+    QVBoxLayout *vbLayout = new QVBoxLayout( this );
+    vbLayout->addWidget( LineEditDir );
+    vbLayout->addWidget( ListWidgetFile );
 
-           connect(LineEditDir,SIGNAL(returnPressed ()),this,SLOT(slotShow()));
-           connect(ListWidgetFile,SIGNAL(itemDoubleClicked(QListWidgetItem * )),this,SLOT(slotShowDir(QListWidgetItem *)));
+    connect(LineEditDir,SIGNAL(returnPressed ()),this,SLOT(slotShow()));
+    connect(ListWidgetFile,SIGNAL(itemDoubleClicked(QListWidgetItem * )),this,SLOT(slotShowDir(QListWidgetItem *)));
 
-           QString root = "/";
-           QDir rootDir(root);
-           QStringList string;
-           string << "*" ;
-           QFileInfoList list=rootDir.entryInfoList (string);
-           showFileInfoList(list);
+    QString root = "/";
+    QDir rootDir(root);
+    QStringList string;
+    string << "*" ;
+    QFileInfoList list=rootDir.entryInfoList (string);
+    showFileInfoList(list);
 }
 
 FlodsView::~FlodsView()
@@ -37,46 +37,46 @@ FlodsView::~FlodsView()
 void FlodsView::showFileInfoList(QFileInfoList list)
 {
     ListWidgetFile->clear();
-        for(unsigned int i=0;i<list.count();i++)
+    for(unsigned int i=0;i<list.count();i++)
+    {
+        QFileInfo tmpFileInfo=list.at(i);
+        if((tmpFileInfo.isDir()))
         {
-            QFileInfo tmpFileInfo=list.at(i);
-            if((tmpFileInfo.isDir()))
-            {
-                QIcon icon(":/res/dir.png");
+            QIcon icon(":/res/dir.png");
 
-                QString fileName=tmpFileInfo.fileName();
+            QString fileName=tmpFileInfo.fileName();
 
-                QListWidgetItem *tmp=new QListWidgetItem (icon,fileName);
+            QListWidgetItem *tmp=new QListWidgetItem (icon,fileName);
 
-                ListWidgetFile->addItem(tmp);
+            ListWidgetFile->addItem(tmp);
 
-            }
-            else if(tmpFileInfo.isFile())
-            {
-                QIcon icon(":/res/file.png");
-                QString fileName=tmpFileInfo.fileName();
-                QListWidgetItem *tmp=new QListWidgetItem (icon,fileName);
-                ListWidgetFile->addItem(tmp);
-
-            }
         }
+        else if(tmpFileInfo.isFile())
+        {
+            QIcon icon(":/res/file.png");
+            QString fileName=tmpFileInfo.fileName();
+            QListWidgetItem *tmp=new QListWidgetItem (icon,fileName);
+            ListWidgetFile->addItem(tmp);
+
+        }
+    }
 }
 
 void FlodsView::slotShowDir(QListWidgetItem *item)
 {
     QString str=item->text();
-        QDir dir;
-        dir.setPath(LineEditDir->text());
-        dir.cd(str);
-        LineEditDir->setText(dir.absolutePath());
-        slotShow(dir);
+    QDir dir;
+    dir.setPath(LineEditDir->text());
+    dir.cd(str);
+    LineEditDir->setText(dir.absolutePath());
+    slotShow(dir);
 }
 
 void FlodsView::slotShow(QDir dir)
 {
     QStringList string;
-        string << "*" ;
-        qDebug() << "FileViewer::slotShow count = " << dir.count();
-        QFileInfoList list=dir.entryInfoList (string,QDir::AllEntries,QDir::DirsFirst);
-        showFileInfoList(list);
+    string << "*" ;
+    qDebug() << "FileViewer::slotShow count = " << dir.count();
+    QFileInfoList list=dir.entryInfoList (string,QDir::AllEntries,QDir::DirsFirst);
+    showFileInfoList(list);
 }
